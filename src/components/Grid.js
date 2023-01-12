8731ae4bf45d7311b4b25d35ef24eb32bd93b252
snehafarkya <snhafarkya@gmail.com>
2023-01-12 14:29:09 +0530

    web3 services added

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import arrow from './assets/arrow.png'
+import arrow from './assets/star.webp'
 
 export default function Grid() {
   return (
@@ -7,49 +7,49 @@ export default function Grid() {
     <div className='grid grid-cols-2 gap-4 place-items-center content-center '>
       {/* #050a36 */}
       {/* card1 */}
-      <div class="flex justify-center gap-x-4" style={{background:"#050a36"}}>
-        <div class="block p-6 rounded-lg shadow-lg max-w-xs">
-            <h5 class="text-gray-100 text-xl leading-tight font-medium mb-2">Web3 services for every app</h5>
+      <div class="info-card flex justify-center gap-x-4" >
+        <div class="block p-6 pl-14 rounded-lg shadow-lg max-w-xs">
+            <h5 class="text-gray-100 text-xl  leading-tight font-medium mb-2">Web3 identities</h5>
             <p class="text-gray-400 text-base mb-4">
               Some quick example text to build on the card title and make up the bulk of the card's
               content.
             </p>
         </div>
-        <img src={arrow} alt="" className='w-24 h-24 mt-10' />
+        <img src={arrow} alt="" className='w-10 h-10 mt-5' />
       </div>
 
       {/* card2 */}
-      <div class="flex justify-center gap-x-4" style={{background:"#050a36"}}>
-        <div class="block p-6 rounded-lg shadow-lg max-w-xs">
-            <h5 class="text-gray-100 text-xl leading-tight font-medium mb-2">Web3 services for every app</h5>
+      <div class="info-card flex justify-center gap-x-4" >
+        <div class="block p-6 pl-14  rounded-lg shadow-lg max-w-xs">
+            <h5 class="text-gray-100 text-xl leading-tight font-medium mb-2">Web3 storage</h5>
             <p class="text-gray-400 text-base mb-4">
               Some quick example text to build on the card title and make up the bulk of the card's
               content.
             </p>
         </div>
-        <img src={arrow} alt="" className='w-24 h-24 mt-10' />
+        <img src={arrow} alt="" className='w-10 h-10 mt-5' />
       </div>
       {/* card 3 */}
-      <div class="flex justify-center gap-x-4" style={{background:"#050a36"}}>
-        <div class="block p-6 rounded-lg shadow-lg max-w-xs">
-            <h5 class="text-gray-100 text-xl leading-tight font-medium mb-2">Web3 services for every app</h5>
+      <div class="info-card flex justify-center gap-x-4" >
+        <div class="block p-6 pl-14  rounded-lg shadow-lg max-w-xs">
+            <h5 class="text-gray-100 text-xl leading-tight font-medium mb-2">Web3 compute</h5>
             <p class="text-gray-400 text-base mb-4">
               Some quick example text to build on the card title and make up the bulk of the card's
               content.
             </p>
         </div>
-        <img src={arrow} alt="" className='w-24 h-24 mt-10' />
+        <img src={arrow} alt="" className='w-10 h-10 mt-5' />
       </div>
       {/* card 4 */}
-      <div class="flex justify-center gap-x-4" style={{background:"#050a36"}}>
-        <div class="block p-6 rounded-lg shadow-lg max-w-xs">
-            <h5 class="text-gray-100 text-xl leading-tight font-medium mb-2">Web3 services for every app</h5>
+      <div class="info-card flex justify-center gap-x-4" >
+        <div class="block p-6 pl-14  rounded-lg shadow-lg max-w-xs">
+            <h5 class="text-gray-100 text-xl leading-tight font-medium mb-2">Web3 hosting</h5>
             <p class="text-gray-400 text-base mb-4">
               Some quick example text to build on the card title and make up the bulk of the card's
               content.
             </p>
         </div>
-        <img src={arrow} alt="" className='w-24 h-24 mt-10' />
+        <img src={arrow} alt="" className='w-10 h-10 mt-5' />
       </div>
     </div>
     </div>
