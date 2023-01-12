6f14137cff4553a37095ca6daa665e83165fa48e
snehafarkya <snhafarkya@gmail.com>
2023-01-12 16:02:35 +0530

    latest updates added

--- src/components/Updateinfocard.js
+++ src/components/Updateinfocard.js
@@ -2,7 +2,7 @@ import React from 'react'
 import launch from './assets/launch.png'
 export default function Updateinfocard() {
   return (
-    <div className='flex justify-center gap-x-5 grid grid-cols-1 gap-y-5'>
+    <div className='md:flex justify-center gap-x-5 grid grid-cols-1 gap-y-5'>
       <div class="flex justify-center p-4 rounded-lg" style={{background:"#050a36"}}>
         <div class="rounded-lg shadow-lg max-w-sm">
           <a href="#!" data-mdb-ripple="true" data-mdb-ripple-color="light">
